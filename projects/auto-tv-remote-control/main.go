2dd627f272fd20e1ab9d694ed5b1321d63574dd0
Shanghui Yang <shanghui.yang@grabtaxi.com>
2024-06-30 21:48:10 +0800

    refine codes

--- projects/auto-tv-remote-control/main.go
+++ projects/auto-tv-remote-control/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/shanghuiyang/rpi-devices/dev"
@@ -16,19 +18,35 @@ const (
 	pinBuz      = 26
 )
 
-var (
-	tvOK     = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xCE}
-	tvLeft   = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0x99}
-	tvRight  = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xC1}
-	tvUp     = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xCA}
-	tvDown   = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xD2}
-	tvBack   = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xC5}
-	tvSleep  = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xDC}
-	tvRed    = []byte{0xB3, 0x4C, 0x84}
-	tvGreen  = []byte{0xB3, 0x4C, 0x89}
-	tvYellow = []byte{0xB3, 0x4C, 0xD9}
-	tvBlue   = []byte{0xB3, 0x4C, 0x96}
+const (
+	OK     = "OK"
+	LEFT   = "LEFT"
+	RIGHT  = "RIGHT"
+	UP     = "UP"
+	DOWN   = "DOWN"
+	BACK   = "BACK"
+	SLEEP  = "SLEEP"
+	RED    = "RED"
+	GREEN  = "GREEN"
+	YELLOW = "YELLOW"
+	BLUE   = "BLUE"
 )
+
+type code []byte
+
+var codes = map[string]code{
+	OK:     {0xA1, 0xF1, 0xB3, 0x4C, 0xCE},
+	LEFT:   {0xA1, 0xF1, 0xB3, 0x4C, 0x99},
+	RIGHT:  {0xA1, 0xF1, 0xB3, 0x4C, 0xC1},
+	UP:     {0xA1, 0xF1, 0xB3, 0x4C, 0xCA},
+	DOWN:   {0xA1, 0xF1, 0xB3, 0x4C, 0xD2},
+	BACK:   {0xA1, 0xF1, 0xB3, 0x4C, 0xC5},
+	SLEEP:  {0xA1, 0xF1, 0xB3, 0x4C, 0xDC},
+	RED:    {0xB3, 0x4C, 0x84},
+	GREEN:  {0xB3, 0x4C, 0x89},
+	YELLOW: {0xB3, 0x4C, 0xD9},
+	BLUE:   {0xB3, 0x4C, 0x96},
+}
 
 var (
 	ir       *dev.IRCoder
@@ -47,31 +65,39 @@ func main() {
 
 	log.Printf("get ready for working")
 	for {
-		data, err := ir.Read()
-		if err != nil || len(data) != 3 {
+		recv, err := ir.Read()
+		if err != nil || len(recv) != 3 {
 			util.DelayMs(100)
 			continue
 		}
 
-		if data[0] == tvRed[0] && data[1] == tvRed[1] && data[2] == tvRed[2] {
+		redBtn := codes[RED]
+		redBtnPressed := recv[0] == redBtn[0] && recv[1] == redBtn[1] && recv[2] == redBtn[2]
+		if redBtnPressed {
+			log.Printf("red button was pressed")
 			go buz.Beep(1, 100)
 			play()
 			util.DelaySec(1)
 		}
 
-		if data[0] == tvBlue[0] && data[1] == tvBlue[1] && data[2] == tvBlue[2] {
+		blueBtn := codes[BLUE]
+		blueBtnPressed := recv[0] == blueBtn[0] && recv[1] == blueBtn[1] && recv[2] == blueBtn[2]
+		if blueBtnPressed {
+			log.Printf("blue button was pressed")
 			go buz.Beep(1, 100)
 			sleep()
 			util.DelaySec(1)
 		}
 
 		if playBtn.Pressed() {
+			log.Printf("play button was pressed")
 			go buz.Beep(1, 100)
 			play()
 			util.DelaySec(1)
 		}
 
 		if sleepBtn.Pressed() {
+			log.Printf("sleep button was pressed")
 			go buz.Beep(1, 100)
 			sleep()
 			util.DelaySec(1)
@@ -84,18 +110,19 @@ func main() {
 
 func play() {
 	log.Printf("going to play tv")
-	cmds := [][]byte{tvLeft, tvOK, tvBack, tvOK, tvDown, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvDown, tvOK}
+	ops := []string{LEFT, OK, BACK, OK, DOWN, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, DOWN, OK}
 	secs := []int{2, 2, 15, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 5}
-	for i, cmd := range cmds {
+	for i, op := range ops {
 		log.Printf("delay %v sec", secs[i])
 		util.DelaySec(time.Duration(secs[i]))
-		if err := ir.Send(cmd); err != nil {
+		code := codes[op]
+		if err := ir.Send(code); err != nil {
 			go buz.Beep(1, 2000)
 			log.Printf("error: %v", err)
 			break
 		}
 		go buz.Beep(1, 100)
-		log.Printf("sent %v\n", cmd)
+		log.Printf("sent: [%v], code: %v\n", op, code)
 	}
 	go buz.Beep(3, 100)
 	log.Printf("done")
@@ -103,19 +130,28 @@ func play() {
 
 func sleep() {
 	log.Printf("going to sleep")
-	cmds := [][]byte{tvBack, tvBack, tvBack, tvBack, tvLeft, tvOK, tvSleep}
+	ops := []string{BACK, BACK, BACK, BACK, LEFT, OK, SLEEP}
 	secs := []int{3, 3, 3, 3, 3, 3, 3}
-	for i, cmd := range cmds {
+	for i, op := range ops {
 		log.Printf("delay %v sec", secs[i])
 		util.DelaySec(time.Duration(secs[i]))
-		if err := ir.Send(cmd); err != nil {
+		code := codes[op]
+		if err := ir.Send(code); err != nil {
 			go buz.Beep(1, 2000)
 			log.Printf("error: %v", err)
 			break
 		}
 		go buz.Beep(1, 100)
-		log.Printf("sent %v\n", cmd)
+		log.Printf("sent: [%v], code: %v\n", op, code)
 	}
 	go buz.Beep(3, 100)
 	log.Printf("done")
+}
+
+func (c code) String() string {
+	var s []string
+	for _, b := range c {
+		s = append(s, fmt.Sprintf("0x%X", b))
+	}
+	return strings.Join(s, ", ")
 }
