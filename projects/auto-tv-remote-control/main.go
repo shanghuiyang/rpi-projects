45021b3f6c48270be768564864890b0c76335f40
Shanghui Yang <shanghui.yang@grabtaxi.com>
2024-06-30 17:29:49 +0800

    improve workflow

--- projects/auto-tv-remote-control/main.go
+++ projects/auto-tv-remote-control/main.go
@@ -17,15 +17,17 @@ const (
 )
 
 var (
-	tvOK    = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xCE}
-	tvLeft  = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0x99}
-	tvRight = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xC1}
-	tvUp    = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xCA}
-	tvDown  = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xD2}
-	tvBack  = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xC5}
-	tvSleep = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xDC}
-	tvRed   = []byte{0xB3, 0x4C, 0x84}
-	tvGreen = []byte{0xB3, 0x4C, 0x89}
+	tvOK     = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xCE}
+	tvLeft   = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0x99}
+	tvRight  = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xC1}
+	tvUp     = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xCA}
+	tvDown   = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xD2}
+	tvBack   = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xC5}
+	tvSleep  = []byte{0xA1, 0xF1, 0xB3, 0x4C, 0xDC}
+	tvRed    = []byte{0xB3, 0x4C, 0x84}
+	tvGreen  = []byte{0xB3, 0x4C, 0x89}
+	tvYellow = []byte{0xB3, 0x4C, 0xD9}
+	tvBlue   = []byte{0xB3, 0x4C, 0x96}
 )
 
 var (
@@ -51,13 +53,13 @@ func main() {
 			continue
 		}
 
-		if data[0] == tvGreen[0] && data[1] == tvGreen[1] && data[2] == tvGreen[2] {
+		if data[0] == tvRed[0] && data[1] == tvRed[1] && data[2] == tvRed[2] {
 			go buz.Beep(1, 100)
 			play()
 			util.DelaySec(1)
 		}
 
-		if data[0] == tvRed[0] && data[1] == tvRed[1] && data[2] == tvRed[2] {
+		if data[0] == tvBlue[0] && data[1] == tvBlue[1] && data[2] == tvBlue[2] {
 			go buz.Beep(1, 100)
 			sleep()
 			util.DelaySec(1)
@@ -74,7 +76,7 @@ func main() {
 			sleep()
 			util.DelaySec(1)
 		}
-		
+
 		util.DelayMs(100)
 	}
 
@@ -82,8 +84,8 @@ func main() {
 
 func play() {
 	log.Printf("going to play tv")
-	cmds := [][]byte{tvLeft, tvOK, tvUp, tvOK, tvDown, tvRight, tvOK, tvOK}
-	secs := []int{2, 2, 20, 3, 3, 3, 3, 5}
+	cmds := [][]byte{tvLeft, tvOK, tvBack, tvOK, tvDown, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvRight, tvDown, tvOK}
+	secs := []int{2, 2, 15, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 5}
 	for i, cmd := range cmds {
 		log.Printf("delay %v sec", secs[i])
 		util.DelaySec(time.Duration(secs[i]))
@@ -101,8 +103,8 @@ func play() {
 
 func sleep() {
 	log.Printf("going to sleep")
-	cmds := [][]byte{tvBack, tvBack, tvBack, tvBack, tvBack, tvLeft, tvOK, tvSleep}
-	secs := []int{3, 3, 3, 3, 3, 3, 3, 3}
+	cmds := [][]byte{tvBack, tvBack, tvBack, tvBack, tvLeft, tvOK, tvSleep}
+	secs := []int{3, 3, 3, 3, 3, 3, 3}
 	for i, cmd := range cmds {
 		log.Printf("delay %v sec", secs[i])
 		util.DelaySec(time.Duration(secs[i]))
